77f91d404b2b0da592cfe2a2c81c955a8c7a33a4
pr-estt <mcadamsrd@gmail.com>
2026-02-15 10:25:36 +0000

    refactor: adjusted navbar width

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Navbar() {
   return (
-    <nav className="bg-foreground from-background to-muted/20 flex items-center justify-between py-6 px-6">
+    <nav className="bg-foreground from-background to-muted/20 flex items-center justify-between py-6 px-20">
       <div className="text-xl font-bold text-background">Logo</div>
       <div className=" space-x-4">
         <Link href="#about" className="text-background hover:text-gray-900">
