a1b84ee67a9fbbc0a5febf04c2e4db81d67b2cdc
pr-estt <mcadamsrd@gmail.com>
2026-02-16 16:08:13 +0000

    refactor: added logo to navbar

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo.JPG.png"
 
 
 export function Navbar() {
   return (
-    <nav className="bg-foreground from-background to-muted/20 flex items-center justify-between py-6 px-20">
-      <div className="text-xl font-bold text-background">Logo</div>
+    <nav className="bg-background from-background to-muted/20 flex items-center justify-between px-20">
+      <div className="text-xl font-bold text-foreground">
+        <Image src={logo} alt="Logo" width={100} height={40} />
+      </div>
       <div className=" space-x-4">
-        <Link href="#about" className="text-background hover:text-gray-900">
+        <Link href="#about" className="text-foreground hover:text-gray-900">
           About
         </Link>
-        <Link href="#services" className="text-background hover:text-gray-900">
+        <Link href="#services" className="text-foreground hover:text-gray-900">
           Services
         </Link>
-        <Link href="#contact" className="text-background hover:text-gray-900">
+        <Link href="#contact" className="text-foreground hover:text-gray-900">
           Contact
         </Link>
       </div>
