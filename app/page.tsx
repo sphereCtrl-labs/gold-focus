9180d4815372549f35c5b94d5f71475db4a8cefa
pr-estt <mcadamsrd@gmail.com>
2026-02-16 18:22:53 +0000

    landing page ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { TestimonialSection } from "@/components/landing/clients-section";
+import { ContactSection } from "@/components/landing/contact-section";
 import { HeroSection } from "@/components/landing/hero-section";
 import { Navbar } from "@/components/landing/navbar";
 import { ServicesSection } from "@/components/landing/services-section";
 import { StateSection } from "@/components/landing/stat-section";
-
+import { Footer } from "@/components/landing/footer-section";
 export default function Page() {
   return (
     <main className="min-h-screen">
@@ -10,6 +12,9 @@ export default function Page() {
       <HeroSection />
       <ServicesSection />
       <StateSection />
+      <TestimonialSection />
+      <ContactSection />
+      <Footer/>
     </main>
   );
 }
